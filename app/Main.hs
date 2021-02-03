00241962dcf948a466b9722d72e61b2d025c4aa4
pf03email <pf03@mail.ru>
2021-02-03 18:56:53 +0300

    init

---
+++ app/Main.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Network.Wai (responseLBS, Application)
+import Network.Wai.Handler.Warp (run)
+import Network.HTTP.Types (status200)
+import Network.HTTP.Types.Header (hContentType)
+
+main = do
+    let port = 8080
+    putStrLn $ "Listening on port " ++ show port
+    run port app
+
+app :: Application
+app req f =
+    f $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!"
+
+-- main = do
+--     let port = 8080
+--     putStrLn $ "Listening on port " ++ show port
+--     run port appmain
