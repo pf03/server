e17043b3eacd8e83ae21eee0e67157bd34fa80f6
pf03email <pf03@mail.ru>
2021-02-05 13:58:08 +0300

    postgresDB connected

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 
 import Network.Wai (responseLBS, Application)
 import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Types (status200)
 import Network.HTTP.Types.Header (hContentType)
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.SqlQQ
 
 main = do
-    let port = 8080
-    putStrLn $ "Listening on port " ++ show port
-    run port app
+    let connectDBInfo  = ConnectInfo {connectHost = "127.0.0.1", connectPort = 5432, connectUser = "postgres", connectPassword = "demo", connectDatabase = "server"}
+    conn <- connect connectDBInfo
+    -- let port = 80
+    -- putStrLn $ "Listening on port " ++ show port
+    -- run port app
+    query_ conn [sql| SELECT id, first_name FROM employee |] :: IO [(Int, String)]
 
 app :: Application
 app req f =
