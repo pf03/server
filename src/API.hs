8314806e073c1f91024ac6854dcec74dbb490954
pf03email <pf03@mail.ru>
2021-03-21 16:36:22 +0300

    test3

--- src/API.hs
+++ src/API.hs
@@ -35,5 +35,5 @@ data APIType = Post | User | Author | Category | Tag | Draft | Comment | Photo |
 -- instance Read API where
 --     readsPrec  = undefined --надо разобраться
 
---хорошие изменения 2
---плохие изменения 2
+--хорошие изменения 3
+--плохие изменения 3
