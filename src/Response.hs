c89ca1a56e400371881ffa95ff2a6f6a969d1729
pf03email <pf03@mail.ru>
2021-04-08 11:08:02 +0300

    Перенес модуль DB в модуль Response, так как по смыслу больше подходит

--- src/Response.hs
+++ src/Response.hs
@@ -8,7 +8,7 @@ import Network.HTTP.Types
 import Network.HTTP.Types.Header (hContentType)
 import qualified System.Console.ANSI as Color
 
-import Data.Aeson
+import Data.Aeson hiding (encode)
 
 --import qualified Data.ByteString.Lazy as L
 
@@ -32,19 +32,28 @@ import Control.Monad.Trans.Except
 --import Database.PostgreSQL.Simple
 --import Network.HTTP.Types.URI
 import Data.Aeson.Encode.Pretty
+import qualified API
+import qualified Params
+import qualified State as S
+import API
+import JSON
+import qualified Network.HTTP.Types.URI as HTTP
+import qualified Upload
+import qualified Auth
+import qualified Select
+import qualified Update
+import qualified Delete
+import qualified Insert
+import Router
+
+import Data.Typeable
 
 
---Модуль DB перенести в Response, так как по смыслу больше подходит
 get :: Request -> T Response
 get req = do
     Log.setSettings Color.Blue  True "Response.get"
     Log.dataT Log.Debug req
-    -- let pathInfo = Wai.pathInfo req
-    -- let queryString = Wai.queryString req
-    -- saveBinary req
-    --undefined
-    -- json <- DB.getJSON (rawPathInfo req) pathInfo queryString
-    json <- DB.getJSON_ req
+    json <- Response.getJSON_ req
     Response.json json
 
 json :: LC.ByteString -> T Response
@@ -59,3 +68,182 @@ errorHandler e = do
         else convertL . show $ e
     Wai.responseLBS status [(hContentType, "text/plain")] text
 
+
+
+---------------------------------- бывший модуль DB---------------------------------------------------------------
+
+
+getJSON_ :: Request -> T LC.ByteString
+getJSON_ req = do
+    S.resetChanged
+    Log.setSettings Color.Blue True "Response.getJSONwithUpload" 
+    Log.funcT Log.Debug "..."
+    let (rawPathInfo, pathInfo, qs) = ( Wai.rawPathInfo req, Wai.pathInfo req, Wai.queryString req)
+
+    requestBody <- toT $ streamOne (getRequestBodyChunk req)
+    Log.debugT requestBody
+    --эту функцию можно использовать для тестирования и эмуляции запросов
+    let qsBody = parseQuery requestBody
+    --Log.debugT req  
+    logT $ Auth.auth req
+
+    Auth.auth req
+    auth <- S.getAuth
+
+    api@(API apiType queryTypes) <- logT $ router rawPathInfo pathInfo auth
+    
+    params <- if apiType `elem` [API.Auth, API.Delete, API.Insert, API.Update] 
+        then catchT (logT $ Params.parseParams qsBody api) $
+            \(RequestError e) -> throwT $ RequestError $ template "{0}.\n Внимание: параметры для данного запроса должны передаваться в теле запроса методом x-www-form-urlencoded" [e] 
+        else catchT (logT $ Params.parseParams qs api) $
+            \(RequestError e) -> throwT $ RequestError $ template "{0}.\n Внимание: параметры для данного запроса должны передаваться в строке запроса" [e] 
+    S.setParams params
+    getJSON api req
+
+--эта обертка для удобства тестирования--эту обертку перенести в Test
+getJSONTest :: BC.ByteString -> PathInfo -> HTTP.Query -> HTTP.Query -> RequestHeaders -> T LC.ByteString
+getJSONTest rawPathInfo pathInfo qs qsBody headers = do
+    S.resetCache --это нужно только для тестов, в сервере и так трансформер возрождается заново при каждом запросе
+    --Log.setSettings Color.Blue True "Response.getJSONTest" 
+    --Log.funcT Log.Debug "..."
+    let req  = Wai.defaultRequest {requestHeaders = headers}
+    Auth.auth req
+    auth <- S.getAuth
+    api@(API apiType queryTypes) <- logT $ router rawPathInfo pathInfo auth
+    params <- if apiType `elem` [API.Auth, API.Delete, API.Insert, API.Update] 
+        then catchT (logT $ Params.parseParams qsBody api) $
+            \(RequestError e) -> throwT $ RequestError $ template "{0}.\n Внимание: параметры для данного запроса должны передаваться в теле запроса методом x-www-form-urlencoded" [e] 
+        else catchT (logT $ Params.parseParams qs api) $
+            \(RequestError e) -> throwT $ RequestError $ template "{0}.\n Внимание: параметры для данного запроса должны передаваться в строке запроса" [e] 
+    S.setParams params
+    getJSON api req
+
+getJSON:: API -> Request -> T LC.ByteString
+getJSON api req = case api of
+    API Auth [] -> encode Auth.login
+
+    API Upload [API.Photo] -> encode $ Upload.photo req
+
+    --апи, которые не возвращают количество измененных строк
+    --может publish сделать отдельным querytype?
+    API Insert [API.User] -> encode Insert.user
+    API Insert [API.Author] -> encode Insert.author
+    API Insert [API.Category] -> encode Insert.category
+    API Insert [API.Tag] -> encode Insert.tag
+    API Insert [API.Draft] -> encode Insert.draft
+    API Insert [API.Draft, Id n, API.Post] -> encode $ Insert.publish n
+    API Insert [API.Post, Id n, API.Comment] -> encode $ Insert.comment n
+
+    API Update [API.User, Id n] -> encode $ Update.user n
+    API Update [API.Author, Id n] -> encode $ Update.author n
+    API Update [API.Category, Id n] -> encode $ Response.updateCategory n
+    API Update [API.Tag, Id n] -> encode $ Update.tag n
+    API Update [API.Draft, Id n] -> encode $ Update.draft n
+    API Update [API.Post, Id n] -> encode $ Update.post n
+
+    API Delete [API.User, Id n] -> encode $ Delete.user n
+    API Delete [API.Author, Id n] -> encode $ Delete.author n
+    API Delete [API.Category, Id n] -> encode $ Delete.category n
+    API Delete [API.Tag, Id n] -> encode $ Delete.tag n
+    API Delete [API.Post, Id n] -> encode $ Delete.post n
+    API Delete [API.Draft, Id n] -> encode $ Delete.draft n
+    API Delete [API.Comment, Id n] -> encode $ Delete.comment n
+
+    --апи, которые возвращают результат
+    API Select [API.User] -> encode Select.users
+    API Select [API.Author] -> encode $ evalAuthors <$> Select.authors
+    API Select [API.Category] -> encode Response.getCategories
+    API Select [API.Tag] -> encode Select.tags
+    API Select [API.Post] -> encode Response.getPosts
+    API Select [API.Draft] -> encode Response.getDrafts
+    API Select [API.Post, Id n, API.Comment] -> encode $ evalComments <$> Select.comments n
+
+    API SelectById [API.User, Id n] -> encode $ Select.user n
+    API SelectById [API.Author, Id n] -> encode $ (evalAuthor <$>) <$> Select.author n
+    API SelectById [API.Category, Id n] -> encode $ Response.getCategory n
+    API SelectById [API.Tag, Id n] -> encode $ Select.tag n
+    API SelectById [API.Post, Id n] -> encode $ Response.getPost n
+    API SelectById [API.Draft, Id n] -> encode $ Response.getDraft n
+
+encode :: (Typeable a, ToJSON a) => T a -> T LC.ByteString
+encode ta = do
+    a <- ta
+    json <- if showType a == "()" then encodePretty <$> S.getChanged else encodePretty <$> ta
+    writeResponseJSON json
+    return json
+
+showType :: Typeable a => a -> String
+showType = show . typeOf
+
+getPosts :: T [Post]
+getPosts = do
+    --эта строка первая, чтобы не перезаписывать настройки лога
+    categories <- Response.getAllCategories
+    Log.setSettings Color.Blue True "Response.getPosts" 
+    Log.funcT Log.Debug "..."
+    S.modifyParams $ evalParams categories
+    selectPosts <- Select.posts
+    toT $ JSON.evalPosts categories selectPosts
+
+getDrafts :: T [Draft]
+getDrafts = do
+    --эта строка первая, чтобы не перезаписывать настройки лога
+    categories <- Response.getAllCategories
+    Log.setSettings Color.Blue True "Response.getDrafts" 
+    Log.funcT Log.Debug "..."
+    S.modifyParams $ evalParams categories
+    selectDrafts <- Select.drafts
+    toT $ JSON.evalDrafts categories selectDrafts
+
+getPost :: Int -> T (Maybe Post)
+getPost pid = do
+    --эта строка первая, чтобы не перезаписывать настройки лога
+    categories <- Response.getAllCategories
+    Log.setSettings Color.Blue True "Response.getPost" 
+    Log.funcT Log.Debug "..."
+    selectPosts <- Select.post pid
+    jsonPosts <- toT $ JSON.evalPosts categories selectPosts
+    return $ listToMaybe jsonPosts --проверить как это работает. evalUnitedPosts должно объединять все в один пост
+
+getDraft :: Int -> T (Maybe Draft)
+getDraft pid = do
+    --эта строка первая, чтобы не перезаписывать настройки лога
+    categories <- Response.getAllCategories
+    Log.setSettings Color.Blue True "Response.getDraft" 
+    Log.funcT Log.Debug "..."
+    selectDrafts <- Select.draft pid
+    jsonDrafts <- toT $ JSON.evalDrafts categories selectDrafts
+    return $ listToMaybe jsonDrafts --проверить как это работает. evalUnitedDrafts должно объединять все в один пост
+
+getAllCategories :: T [Category]
+getAllCategories = do
+    Log.setSettings Color.Blue True "Response.getAllCategories"
+    Log.funcT Log.Debug "..."
+    allCategories <- Select.allCategories
+    toT $ evalCategories allCategories allCategories
+
+getCategories :: T [Category]
+getCategories = do
+    Log.setSettings Color.Blue True "Response.getCategories"
+    Log.funcT Log.Debug "..."
+    allCategories <- Select.allCategories
+    categories <- Select.categories
+    toT $ evalCategories allCategories categories
+
+updateCategory :: Int -> T () 
+updateCategory pid = do 
+    params <- S.getParams 
+    Log.setSettings Color.Blue True "Response.updateCategory"
+    Log.funcT Log.Debug "..."
+    allCategories <- Select.allCategories
+    toT $ checkCyclicCategory pid params allCategories
+    Update.category pid
+
+--эту логику перенести в select??
+getCategory :: Int -> T (Maybe Category)
+getCategory pid = do
+    Log.setSettings Color.Blue True "Response.getCategory"
+    Log.funcT Log.Debug "..."
+    allCats <- Select.allCategories
+    mcat <- Select.category pid
+    toT $ sequenceA $ evalCategory allCats <$> mcat
