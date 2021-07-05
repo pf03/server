bf259475b1e07b65ca80fb8b888941a21fb947dd
pf03email <pf03@mail.ru>
2021-07-05 20:07:42 +0300

    deleted not used functions in test/Lib.hs

--- test/Lib.hs
+++ test/Lib.hs
@@ -1,47 +1,10 @@
 module Lib where
 
-import Control.Monad.State.Lazy (State, runState)
-import Test.Hspec
-  ( Expectation,
-    HasCallStack,
-    shouldBe,
-    shouldSatisfy,
-  )
-
------------------------------Multiple cases-------------------------------------
---ALL of cases SHOULD BE equal to one result
-allShouldBe :: (HasCallStack, Show a, Eq a) => [a] -> a -> Expectation
-allShouldBe cases result = eachShouldBe cases (replicate (length cases) result)
-
---EACH of cases SHOULD BE equal to each of results
-eachShouldBe :: (HasCallStack, Show a, Eq a) => [a] -> [a] -> Expectation
-eachShouldBe = biMapM_ shouldBe
+import Test.Hspec ( HasCallStack, shouldSatisfy, Expectation )
 
 --ALL of cases SHOULD SATISFY to one predicate
 allShouldSatisfy :: (HasCallStack, Show a) => [a] -> (a -> Bool) -> Expectation
 allShouldSatisfy cases f = biMapM_ shouldSatisfy cases (replicate (length cases) f)
-
------------------------------State cases---------------------------------------
--- Can't use biMapM_ in this functions, because they do not consider State effect
--- ALL EVAL STATES of cases SHOULD BE equal to one result WITH INITIAL STATE
-allEvalStatesShouldBe :: (HasCallStack, Show a, Eq a) => [State s a] -> (a, s) -> Expectation
-allEvalStatesShouldBe states (result, initialState) = eachEvalStateShouldBe states (replicate (length states) result, initialState)
-
---EACH EVAL STATE of cases SHOULD BE equal to each of results WITH INITIAL STATE
-eachEvalStateShouldBe :: (HasCallStack, Show a, Eq a) => [State s a] -> ([a], s) -> Expectation
-eachEvalStateShouldBe [] ([], _) = return ()
-eachEvalStateShouldBe (s : ss) (r : rs, initialState) = do
-  let (a, modifiedState) = runState s initialState
-  a `shouldBe` r
-  eachEvalStateShouldBe ss (rs, modifiedState)
-eachEvalStateShouldBe _ _ = fail "lists of tests and answers must have equal lengths"
-
---EVAL STATE of case SHOULD BE equal to result WITH INITIAL STATE
-evalStateShouldBe :: (HasCallStack, Show a, Eq a) => State s a -> (a, s) -> Expectation
-evalStateShouldBe state (result, initialState) = eachEvalStateShouldBe [state] ([result], initialState)
-
-withInitialState :: a -> b -> (a, b)
-withInitialState = (,)
 
 -- This is not equal to base Data.Bifoldable.bimapM_
 biMapM_ :: Monad m => (a -> b -> m c) -> [a] -> [b] -> m ()
