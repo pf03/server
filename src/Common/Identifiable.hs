8f6e295556254c68e2f69a9ed46ac62f29f3e6db
pf03email <pf03@mail.ru>
2021-09-07 11:48:41 +0300

    Common.Identifiable delete unused functions

--- src/Common/Identifiable.hs
+++ src/Common/Identifiable.hs
@@ -8,9 +8,6 @@ class Identifiable a where
 findById :: Identifiable a => Int -> [a] -> Maybe a
 findById wantedId = find (\a -> getId a == wantedId)
 
-existId :: Identifiable a => Int -> [a] -> Bool
-existId wantedId = any (\a -> getId a == wantedId)
-
 -- | Set value only if find the element
 setById :: Identifiable a => a -> [a] -> [a]
 setById updated = map helper
@@ -18,23 +15,7 @@ setById updated = map helper
     wantedId = getId updated
     helper curValue = if getId curValue == wantedId then updated else curValue
 
--- | Set value anyway
-insertById :: Identifiable a => a -> [a] -> [a]
-insertById updated list = res
-  where
-    wantedId = getId updated
-    res =
-      if existId wantedId list
-        then setById updated list
-        else list <> [updated]
-
-updateById :: Identifiable a => Int -> (a -> a) -> [a] -> [a]
-updateById wantedId func = map helper
-  where
-    helper curValue = if getId curValue == wantedId then func curValue else curValue
-
 -- * Should not break sort
-
 updateInsertById :: Identifiable a => (a -> a) -> a -> [a] -> [a]
 updateInsertById func updated list = res
   where
