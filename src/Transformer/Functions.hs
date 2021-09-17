06e82d62ca2a1a026b43b810ed2ac70da7b4bbfb
pf03email <pf03@mail.ru>
2021-09-17 16:30:37 +0300

    add closing db connection for every server request

--- src/Transformer/Functions.hs
+++ src/Transformer/Functions.hs
@@ -1,5 +1,6 @@
 module Transformer.Functions (module Transformer.Functions, runConfig, exceptToMaybe) where
 
+import Database.PostgreSQL.Simple (close)
 import qualified Interface.MError.Exports as Error
 import qualified Logic.IO.Config as Config
 import Transformer.Internal
@@ -17,11 +18,19 @@ runT :: Transformer a -> IO ()
 runT m = runE_ $ do
   config <- runConfig
   connection <- runConnection config
-  _ <- getValue config connection m
-  return ()
+  _ <-
+    getValue config connection m `Error.catchServerError` \err -> do
+      Error.liftEIO $ close connection
+      Error.throwServerError err
+  Error.liftEIO $ close connection
 
 -- | Evaluate value of transformer with error handler
 evalTWithHandler :: Transformer a -> (Error.Error -> a) -> Config.Config -> IO a
 evalTWithHandler m handler config = runEWithHandler handler $ do
   connection <- runConnection config
-  getValue config connection m
+  value <-
+    getValue config connection m `Error.catchServerError` \err -> do
+      Error.liftEIO $ close connection
+      Error.throwServerError err
+  Error.liftEIO $ close connection
+  return value
